a11195ed22ab26b9caa45c8ba78c98d4ae256f85
Sunny Jate <sunnyjate2007@gmail.com>
2024-07-30 21:25:27 +0530

    js update

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,5 +1,14 @@
-function App(){
-    return (
-        <h1>Hello I am componet</h1>
-    )
+import './App.css';
+
+function App() {
+  let a = 2; //it is like JS but we can not able to show it on web page directly. wee need to use '{coponent to show}'
+  return (
+    <div>{a+2} , 
+        {a}+2, 
+       a
+      <h1 className='main'>Hello I am component</h1>
+      <p style={{backgroundColor:"yellow",color:"black"}}>Practice the code</p>
+    </div>//if using multipe tag need to wrap them in the one tag here it is div. Because in React we use jsx files which include javascript and html format in one so we have to do this things.
+  );
 }
+export default App;
