128ce66e36b1e391f107a3563195bc885cb5ebc8
Sunny Jate <sunnyjate2007@gmail.com>
2024-07-31 22:01:04 +0530

    imported the json file

--- 01/src/Mobile.js
+++ 01/src/Mobile.js
@@ -1,22 +1,7 @@
 // Mobile.js
 import React from "react";
 import MobileList from "./MobileList";
-
-const mobs = [
-  {
-    image:
-      "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70",
-    title: "Apple iPhone 15 (Black, 128 GB)",
-    price: "65,000",
-  },
-   {
-    image:
-      "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/m/c/s/-original-imagztn7sycc4h84.jpeg?q=70",
-    title: "SAMSUNG Galaxy S23 FE (Mint, 128 GB)",
-    price: "34,000",
-  },
-]
-
+import mobs from './mobs.json'
 
 export default function Mobile() {
   return (
