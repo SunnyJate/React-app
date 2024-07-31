1ba3a2613ccfdc7f79bf538773ea23f7faf8466b
Sunny Jate <sunnyjate2007@gmail.com>
2024-07-31 21:44:45 +0530

    props added

--- 01/src/MobileList.js
+++ 01/src/MobileList.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./MobileList.css";
 
-export default function MobileList() {
-  return (
-    <div>
-      Hello
-    </div>
-  )
+export default function MobileList(props) {
+  
+
+    return (
+        <div className="main">
+            <img className="image" src={props.image} alt={props.title} />
+            <div>
+                <h2>{props.title}</h2>
+                <p>₹ {props.price}</p>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+    );
 }
-
