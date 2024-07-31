257034dd23a144ea43322ea3edcc4fb34b3fef00
Sunny Jate <sunnyjate2007@gmail.com>
2024-07-31 21:56:04 +0530

    changes in the props mobile.js file

--- 01/src/Mobile.js
+++ 01/src/Mobile.js
@@ -1,23 +1,33 @@
 // Mobile.js
-import React from 'react';
-import MobileList from './MobileList';
+import React from "react";
+import MobileList from "./MobileList";
 
-const mob1 ={
-     image : 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70',
-     title : 'Apple iPhone 15 (Black, 128 GB)',
-     price : '65,000'
-}
-const mob2 ={
-    image : 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/m/c/s/-original-imagztn7sycc4h84.jpeg?q=70',
-    title : 'SAMSUNG Galaxy S23 FE (Mint, 128 GB)',
-    price : '34,000'
-}
+const mobs = [
+  {
+    image:
+      "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70",
+    title: "Apple iPhone 15 (Black, 128 GB)",
+    price: "65,000",
+  },
+   {
+    image:
+      "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/m/c/s/-original-imagztn7sycc4h84.jpeg?q=70",
+    title: "SAMSUNG Galaxy S23 FE (Mint, 128 GB)",
+    price: "34,000",
+  },
+]
+
 
 export default function Mobile() {
   return (
     <div>
-      <MobileList image={mob1.image} title={mob1.title} price={mob1.price} />
-      <MobileList image={mob2.image} title={mob2.title} price={mob2.price} />
+        {mobs.map((ele)=>{
+            return <MobileList 
+            image={ele.image} 
+            title={ele.title} 
+            price={ele.price} 
+            />
+        })}
     </div>
   );
 }
--- 01/src/MobileList.js
+++ 01/src/MobileList.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./MobileList.css";
 
 export default function MobileList(props) {
-  
+  const {image, title, price} = props;
 
     return (
         <div className="main">
-            <img className="image" src={props.image} alt={props.title} />
+            <img className="image" src={image} alt={title} />
             <div>
-                <h2>{props.title}</h2>
-                <p>₹ {props.price}</p>
+                <h2>{title}</h2>
+                <p>₹ {price}</p>
                 <button>Add To Cart</button>
             </div>
         </div>
