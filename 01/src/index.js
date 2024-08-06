4597bff3d2bc40a48729a2b82d8365da41ef41ad
Sunny Jate <sunnyjate2007@gmail.com>
2024-08-06 21:31:24 +0530

    Use of Multiple usestate

--- 01/src/index.js
+++ 01/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Mobile from './Mobile';
 // import App from './App';
-import Data from './Data';
+// import Data from './Data';
 import Form from './Form';
 
 
